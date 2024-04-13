75e0742129b352f859df28726894edb7faa50d7e
rajendra <rajendrammarboyana@gmail.com>
2024-04-13 15:04:45 +0100

    adding

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 component=$1
-ansible-playbook get-secrets.yml -e vault_token=$vault_token
-ansible-playbook -i $component-$env.rajdevops.online, -e env=$env -e role_name=$component expense.yml -e '@secrets.json'
+ansible-playbook -vv get-secrets.yml -e vault_token=$vault_token
+ansible-playbook  -i $component-$env.rajdevops.online, -e env=$env -e role_name=$component expense.yml -e '@secrets.json'
